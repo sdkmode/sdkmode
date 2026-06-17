d5350ae6285c058c38427d6d4a2b93775b43e93c
Heidi Hill <heidi@heidi.codes>
2026-06-17 16:14:04 +0100

    feat: implement github auth

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,36 +1,74 @@
-use core::future::ready;
 use deno_error::JsErrorBox;
 use deno_fetch::{ReqBody, RequestBuilder};
 use http;
+use std::collections::HashMap;
 use std::pin::Pin;
+use std::rc::Rc;
 use std::sync::Arc;
+use tokio::process::Command;
+use tokio::sync::Mutex;
 use url::Url;
 
 trait Sdk: Send + Sync {
     fn url(&self) -> Url;
-    fn oauth<'a>(&'a self) -> Pin<Box<dyn Future<Output = Result<(), JsErrorBox>> + Send + 'a>>;
+    fn auth<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<(), JsErrorBox>> + Send + 'a>>;
+    fn is_authed(&self) -> bool;
+    fn auth_header(&self) -> Option<String>;
+    fn cookies(&self) -> Option<HashMap<String, String>>;
 }
 
-struct GitHub;
+struct GitHub {
+    token: Option<String>,
+}
+
+impl GitHub {
+    fn new() -> Self {
+        Self { token: None }
+    }
+}
 
 impl Sdk for GitHub {
     fn url(&self) -> Url {
         Url::parse("https://api.github.com").unwrap()
     }
 
-    fn oauth<'a>(&'a self) -> Pin<Box<dyn Future<Output = Result<(), JsErrorBox>> + Send + 'a>> {
-        Box::pin(ready(Ok(())))
+    fn auth<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<(), JsErrorBox>> + Send + 'a>> {
+        Box::pin(async move {
+            self.token = String::from_utf8(
+                Command::new("gh")
+                    .arg("auth")
+                    .arg("token")
+                    .output()
+                    .await
+                    .or_else(|e| Err(JsErrorBox::from_err(e)))?
+                    .stdout
+                    .to_vec(),
+            )
+            .ok();
+            Ok(())
+        })
+    }
+
+    fn is_authed(&self) -> bool {
+        self.token.is_some()
+    }
+
+    fn auth_header(&self) -> Option<String> {
+        self.token.as_ref().map(|t| format!("Bearer {}", t.trim()))
+    }
+    fn cookies(&self) -> Option<HashMap<String, String>> {
+        Some(HashMap::<String, String>::new())
     }
 }
 
 struct RequestInterceptor {
-    sdks: Vec<Box<dyn Sdk>>,
+    sdks: Vec<Arc<Mutex<dyn Sdk>>>,
 }
 
 impl RequestInterceptor {
     fn new() -> Self {
         Self {
-            sdks: vec![Box::new(GitHub)],
+            sdks: vec![Arc::new(Mutex::new(GitHub::new()))],
         }
     }
 }
@@ -41,13 +79,53 @@ impl RequestBuilder for RequestInterceptor {
         request: &'a mut http::Request<ReqBody>,
     ) -> Pin<Box<dyn Future<Output = Result<(), JsErrorBox>> + Send + 'a>> {
         Box::pin(async move {
-            let sdk = self
-                .sdks
-                .iter()
-                .find(|sdk| sdk.url().host_str() == request.uri().host())
-                .ok_or_else(|| JsErrorBox::generic("Extension not found"))?;
+            let mut sdk = None;
+            for candidate in &self.sdks {
+                if candidate.lock().await.url().host_str() == request.uri().host() {
+                    sdk = Some(candidate.clone());
+                    break;
+                }
+            }
 
-            sdk.oauth().await
+            let sdk = sdk.ok_or_else(|| JsErrorBox::generic("Extension not found"))?;
+
+            let mut sdk = sdk.lock().await;
+
+            if !sdk.is_authed() {
+                sdk.auth().await?;
+            }
+
+            let headers = request.headers_mut();
+
+            headers.remove(http::header::AUTHORIZATION);
+            headers.remove(http::header::COOKIE);
+
+            if let Some(auth_header) = sdk.auth_header() {
+                headers.insert(
+                    http::header::AUTHORIZATION,
+                    auth_header
+                        .parse::<http::HeaderValue>()
+                        .or_else(|e| Err(JsErrorBox::generic(e.to_string())))?,
+                );
+            }
+
+            if let Some(cookies) = sdk.cookies() {
+                if !cookies.is_empty() {
+                    let cookie_str = cookies
+                        .iter()
+                        .map(|(k, v)| format!("{k}={v}"))
+                        .collect::<Vec<_>>()
+                        .join("; ");
+                    headers.insert(
+                        http::header::COOKIE,
+                        cookie_str
+                            .parse::<http::HeaderValue>()
+                            .or_else(|e| Err(JsErrorBox::generic(e.to_string())))?,
+                    );
+                }
+            }
+
+            Ok(())
         })
     }
 }
